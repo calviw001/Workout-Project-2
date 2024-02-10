cc6d8492f75aab7580cd5e17a7bb90783a23a3bd
Calvin Wong <calviw8@uci.edu>
2024-02-09 19:47:44 -0800

    Updated decrpyt_message.py
    The module now has its own version of read_and_create_text_list, which will take an encrypted message from a file and then split up the message into sections based on the key argument.

--- decrpyt_message.py
+++ decrpyt_message.py
@@ -1,12 +1,23 @@
 from pathlib import Path
-
+import math
 
 
 def read_and_create_text_list(file_name, key, a_list, branch):
+    message_length = 0
     encrypted_message = ""
     current_directory = Path.cwd()
     file_path = current_directory / file_name
     f = open(file_path, "r")
     for line in f:
         encrypted_message += line
-    print(encrypted_message)
+        message_length += len(line)
+    f.close()
+    row_length = message_length / key
+    row_length = math.ceil(row_length)
+    for character in encrypted_message:
+        branch += character
+        if len(branch) == row_length:
+            a_list.append(branch)
+            branch = ""
+    a_list.append(branch)
+    print(a_list)
