ca00653b55179c838417d7348d9907dcaede5c9b
Calvin Wong <calviw8@uci.edu>
2024-02-09 17:17:07 -0800

    Added more functions to key_validation.py
    One fuction will check if an integer is positive, and another function checks if an integer is less then an upper limit value. A temporary main function was added to test these functions.

--- key_validation.py
+++ key_validation.py
@@ -1,4 +1,4 @@
-def __is_int(test_key):
+def _is_int(test_key):
     try:
         assert test_key.isdigit()
         int(test_key)
@@ -6,3 +6,25 @@ def __is_int(test_key):
     except (ValueError, AssertionError):
         return False
 
+
+def _is_postive(an_int):
+    return an_int > 0
+
+
+def _check_length(an_int, upper_bound):
+    return an_int < upper_bound
+
+
+# Temporary main function to test private functions
+def main():
+    k = '11'
+    u = 10
+    if _is_int(k):
+        k = int(k)
+        print(_is_postive(k))
+        print(_check_length(k,u))
+    else:
+        print("NO")
+
+
+main()
